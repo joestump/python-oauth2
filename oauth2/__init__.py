905680d424c21821e02684579d5eb982fed2f85d
Joe Stump <joe@joestump.net>
2009-10-21 11:55:39 -0600

    Added proper copyrights.

--- oauth2/__init__.py
+++ oauth2/__init__.py
@@ -1,7 +1,7 @@
 """
 The MIT License
 
-Copyright (c) 2007 Leah Culver
+Copyright (c) 2007 Leah Culver, Joe Stump, Mark Paschal, Vic Fryzel
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

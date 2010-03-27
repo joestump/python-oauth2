9962d31cc78806b2b17cd3dcd2c7bdc8cc0085c0
Joe Stump <joe@joestump.net>
2010-03-27 12:18:08 -0600

    Upped version for release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="oauth2",
-      version="1.1.2",
+      version="1.1.3",
       description="Library for OAuth version 1.0a.",
       author="Joe Stump",
       author_email="joe@simplegeo.com",

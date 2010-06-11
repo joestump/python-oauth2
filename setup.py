fb4bc9f3612ed04d38ec190f327eaf9f6b28a37a
Ian Eure <ian.eure@gmail.com>
2010-06-11 16:09:15 -0700

    Update packaging.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="oauth2",
-      version="1.2.0",
+      version="1.2.1pre",
       description="Library for OAuth version 1.0a.",
       author="Joe Stump",
       author_email="joe@simplegeo.com",

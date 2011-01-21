bd80349e059146e3f0912a581ed1a5fdb87945b7
Simple Geebus <nerds+geebus@simplegeo.com>
2011-01-21 19:52:32 +0000

    Prepared changelog for build 129

--- oauth2/_version.py
+++ oauth2/_version.py
@@ -1,6 +1,6 @@
 # This is the version of this source code.
 
-verstr = "1.4.128"
+verstr = "1.4.129"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)

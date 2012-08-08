7e73de18ca5c778e4ce53db7995e4a8de34ff7e4
Mark Unsworth <markunsworth@gmail.com>
2012-08-08 16:32:35 +0100

    Changed client tests so that the httplib2.http.request calls (with oauth2.Client) are mocked. Had issues with the oauth sandbox not being available and unit tests should not rely on external services.

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -33,6 +33,7 @@ import urllib
 import urlparse
 from types import ListType
 import mock
+from mock import patch
 import httplib2
 
 # Fix for python2.5 compatibility
@@ -1192,30 +1193,40 @@ class TestClient(unittest.TestCase):
             self.fail("Client.__init__() accepted invalid Token.")
         except ValueError:
             pass
-
+    
     def test_access_token_get(self):
         """Test getting an access token via GET."""
-        client = oauth.Client(self.consumer, None)
-        resp, content = client.request(self._uri('request_token'), "GET")
-
-        self.assertEquals(int(resp['status']), 200)
+        
+        with patch('httplib2.Http.request') as mockRequest:
+        
+            mockRequest.return_value = {'status':200},''        
+            client = oauth.Client(self.consumer, None)
+            resp, content = client.request(self._uri('request_token'), "GET")
+            self.assertEquals(int(resp['status']), 200)
 
     def test_access_token_post(self):
-        """Test getting an access token via POST."""
-        client = oauth.Client(self.consumer, None)
-        resp, content = client.request(self._uri('request_token'), "POST")
+        """Test getting an access token via POST."""    
 
-        self.assertEquals(int(resp['status']), 200)
+        with patch('httplib2.Http.request') as mockRequest:
+        
+            mockRequest.return_value = {'status':200},'oauth_token=123&oauth_token_secret=abcd'        
 
-        res = dict(parse_qsl(content))
-        self.assertTrue('oauth_token' in res)
-        self.assertTrue('oauth_token_secret' in res)
+            client = oauth.Client(self.consumer, None)
+            resp, content = client.request(self._uri('request_token'), "POST")
+
+            self.assertEquals(int(resp['status']), 200)
+
+            res = dict(parse_qsl(content))
+            self.assertTrue('oauth_token' in res)
+            self.assertTrue('oauth_token_secret' in res)
 
     def _two_legged(self, method):
         client = oauth.Client(self.consumer, None)
-
-        return client.request(self._uri('two_legged'), method,
-            body=urllib.urlencode(self.body))
+        with patch('httplib2.Http.request') as mockRequest:
+        
+            mockRequest.return_value = {'status':200},'' 
+            return client.request(self._uri('two_legged'), method,
+                body=urllib.urlencode(self.body))
 
     def test_two_legged_post(self):
         """A test of a two-legged OAuth POST request."""

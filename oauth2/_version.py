55fb8f65a26cf2b00bc043aded1756adfaf4c4f1
Tres Seaver <tseaver@palladion.com>
2013-05-28 15:21:13 -0400

    Coverage for o._version.

--- oauth2/_version.py
+++ oauth2/_version.py
@@ -11,8 +11,9 @@ auto_build_num = "211"
 verstr = manual_verstr + "." + auto_build_num
 try:
     from pyutil.version_class import Version as pyutil_Version
-    __version__ = pyutil_Version(verstr)
-except (ImportError, ValueError):
+except (ImportError, ValueError): #pragma NO COVER
     # Maybe there is no pyutil installed.
     from distutils.version import LooseVersion as distutils_Version
     __version__ = distutils_Version(verstr)
+else: #pragma NO COVER
+    __version__ = pyutil_Version(verstr)
